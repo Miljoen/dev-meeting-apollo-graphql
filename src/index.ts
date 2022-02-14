d5c36f3e63ea64d97d117a691da7ffc9ff87f049
Yoeri Moens <yoerimoens@gmail.com>
2022-02-14 22:35:52 +0100

    Hello world setup added for our GraphQL API

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('Hello world!')
+import { createServer } from 'http'
+import express from 'express'
+import { ApolloServer } from 'apollo-server-express'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import dotenv from 'dotenv'
+import { typeDefs } from './schema'
+import { resolvers } from './resolvers'
+
+dotenv.config()
+main()
+
+async function main() {
+    const app = express()
+    const httpServer = createServer(app)
+
+    const schema = makeExecutableSchema({ typeDefs, resolvers })
+    const server = new ApolloServer({ schema })
+
+    await server.start()
+
+    server.applyMiddleware({ app })
+
+    httpServer.listen(process.env.HTTP_PORT, () => {
+        console.log(`🚀 HTTP server ready at :${process.env.HTTP_PORT}${server.graphqlPath}`)
+        console.log(`🚀 Websocket server ready at :${process.env.HTTP_PORT}${server.graphqlPath}`)
+    })
+}
