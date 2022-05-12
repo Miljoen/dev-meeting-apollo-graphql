3c449aca8d8326e4a10535f4059e9304c377ddd7
Yoeri Moens <yoerimoens@gmail.com>
2022-05-12 15:10:37 +0200

    Typo

--- src/type-graphql/resolvers/user-created.ts
+++ src/type-graphql/resolvers/user-created.ts
@@ -1,24 +1,16 @@
 import * as TypeGraphQL from 'type-graphql'
-import graphqlFields from 'graphql-fields'
-import { GraphQLResolveInfo } from 'graphql'
 import { User } from '../../../prisma/generated/type-graphql'
-import {
-    getPrismaFromContext,
-    transformCountFieldIntoSelectRelationsCount,
-    transformFields,
-} from '../../../prisma/generated/type-graphql/helpers'
 import { pubSub } from '../../pub-sub'
-import { Args, Root } from 'type-graphql'
+import { Root } from 'type-graphql'
 
 @TypeGraphQL.Resolver(_of => User)
 export class UserCreatedResolver {
     @TypeGraphQL.Subscription({
-        // topics: "USER_CREATED", // single topic
         subscribe: () => pubSub.asyncIterator(['USER_CREATED']),
     })
     userCreated(
-        @Root() notificationPayload: User,
+        @Root() userPayload: User,
     ): User {
-        return notificationPayload
+        return userPayload
     }
 }
